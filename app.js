2d223248cc80692e1618280fd255179973e8c2b8
ZhenkangZhao <fyfyzzk@hotmail.com>
2017-10-16 19:26:09 -0700

    add acquire, save, load and update access_token part2

--- app.js
+++ app.js
@@ -2,36 +2,29 @@
 
 
 var Koa = require('koa')
-var sha1 = require('sha1')
+var path = require('path')
+var wechat = require('./middle_ware/g') //  middlware
+var util = require('./libs/util')
+var wechat_file = path.join(__dirname, './config/wechat.txt')
 
 var config = {
     wechat: {
         appId: 'wx2a32f51211b880fc',
         appSecret: '8bff89eb2f84cc08cd8dc2c7bd5ef61e',
-        token: 'aadfdadifusaopfupfuepour'
+        token: 'aadfdadifusaopfupfuepour',
+        getAccessToken: function() {
+            return util.readFileAsync(wechat_file)
+        },
+        saveAccessToken: function (data) {
+            data = JSON.stringify(data);
+            return util.writeFileAsync(wechat_file, data);
+        }
     }
 }
 
-var app = new Koa()
+var app = new Koa();
 
-app.use(function *(next) {
-    console.log(this.query)
-
-    var token = config.wechat.token
-    var signature = this.query.signature
-    var nonce = this.query.nonce
-    var timestamp = this.query.timestamp
-    var echostr = this.query.echostr
-    var str = [token, timestamp, nonce].sort().join('');
-    var sha = sha1(str)
-
-    if (sha === signature) {
-        this.body = echostr + ''
-    } else {
-        this.body = 'wrong'
-    }
-})
+app.use(wechat(config.wechat))
 
 app.listen(8080)
 console.log('listen : 8080')
-//console.log(this.query)
